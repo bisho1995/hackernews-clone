92d75107962b94de7cbb1733db7e6435aca038cc
Bisvarup Mukherjee <codeworks0301@gmail.com>
2018-12-22 19:50:40 +0530

    creating login page

--- app/server/routes.js
+++ app/server/routes.js
@@ -4,6 +4,7 @@ const Register = require("./routes/Register");
 
 module.exports = app => {
   app.get("/", Home);
-  app.get("/login", Login);
+  app.get("/login", Login.get);
+  app.post("/login", Login.post);
   app.get("/register", Register);
 };
--- app/server/routes/Login.js
+++ app/server/routes/Login.js
@@ -1,3 +1,7 @@
-module.exports = function(req, res) {
+module.exports.get = function(req, res) {
   res.status(200).render("login");
+};
+module.exports.post = function(req, res) {
+  console.log(req.body);
+  res.status(200).send("Login post");
 };

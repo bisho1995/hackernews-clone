b05050ab9404daca0f7da0ffa302c5f594407b9b
Bisvarup Mukherjee <codeworks0301@gmail.com>
2018-12-23 12:53:23 +0530

    almost done with dashboard

--- app/server/Controllers/api/index.js
+++ app/server/Controllers/api/index.js
@@ -1,43 +1,53 @@
 const moment = require('moment');
+const axios = require('axios');
 
-module.exports = (params) => {
-  let {
+module.exports = params => new Promise((resolve, reject) => {
+    let {
  sort, prefix, page, dateRange, type, query 
 } = params;
-  const url =    sort === 'byDate'
-      ? 'http://hn.algolia.com/api/v1/search_by_date'
-      : 'http://hn.algolia.com/api/v1/search';
-  page = page || '0';
-  query = query || '';
-  let numericFilters = '';
-  switch (dateRange) {
-    case 'all':
-      break;
-    case 'last24h':
-      numericFilters = moment()
-        .subtract(1, 'days')
-        .unix();
-      break;
-    case 'pastWeek':
-      numericFilters = moment()
-        .subtract(1, 'weeks')
-        .unix();
-      break;
-    case 'pastMonth':
-      numericFilters = moment()
-        .subtract(1, 'months')
-        .unix();
-      break;
-    case 'pastYear':
-      numericFilters = moment()
-        .subtract(1, 'years')
-        .unix();
-      break;
-    default:
-      numericFilters = '';
-  }
-  const tags = type;
+    const url =      sort === 'byDate'
+        ? 'http://hn.algolia.com/api/v1/search_by_date'
+        : 'http://hn.algolia.com/api/v1/search';
+    page = page || '0';
+    query = query || '';
+    let numericFilters = '';
+    switch (dateRange) {
+      case 'all':
+        break;
+      case 'last24h':
+        numericFilters = `created_at_i>${moment()
+          .subtract(1, 'days')
+          .unix()}`;
+        break;
+      case 'pastWeek':
+        numericFilters = `created_at_i>${moment()
+          .subtract(1, 'weeks')
+          .unix()}`;
+        break;
+      case 'pastMonth':
+        numericFilters = `created_at_i>${moment()
+          .subtract(1, 'months')
+          .unix()}`;
+        break;
+      case 'pastYear':
+        numericFilters = `created_at_i>${moment()
+          .subtract(1, 'years')
+          .unix()}`;
+        break;
+      default:
+        numericFilters = '';
+    }
+    const tags = type;
 
-  const api = `${url}?query=${query}&tags=${tags}&numericFilters=created_at_i>${numericFilters}&page=${page}`;
-  console.log(api);
-};
+    const api = `${url}?query=${query}&tags=${tags}&numericFilters=${numericFilters}&page=${page}`;
+    axios
+      .get(api)
+      .then(data => data.data)
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
--- app/server/routes/Home.js
+++ app/server/routes/Home.js
@@ -9,8 +9,22 @@ module.exports = async (req, res) => {
     if (!helper.hasAllQs(req.query)) {
       res.status(200).redirect(`/${helper.fillUpQueryParams(req.query)}`);
     } else {
-      api(req.query);
-      res.status(200).render('home');
+      api(req.query)
+        .then((data) => {
+          data.hits.map((hit) => {
+            if (hit.url === null) hit.url = '#';
+            if (hit.points === null) hit.points = '0';
+            if (hit.author === null) hit.author = '';
+            if (hit.num_comments === null) hit.num_comments = '0';
+            return hit;
+          });
+          console.log(data.hits);
+          res.status(200).render('home', { hits: data.hits });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).send('Internal server error');
+        });
     }
   }
 };
